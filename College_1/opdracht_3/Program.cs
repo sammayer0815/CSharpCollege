79a05d8b1a3b5665497f9584da59a1de4f4a6375
Sam Mayer <sammayer0815@gmail.com>
2024-02-06 17:01:56 +0100

    finished opd 3

--- College_1/opdracht_3/Program.cs
+++ College_1/opdracht_3/Program.cs
@@ -31,13 +31,26 @@ namespace opdracht_3
 
             Console.OutputEncoding = System.Text.Encoding.Unicode;
 
-            for (int i = Floors; i > 0; i--)
+            DrawLiftLines(Floors, LiftCount, Lifts, above, below, user, minIndex);
+
+
+
+        }
+
+        static void DrawLiftLines(int Floors, int LiftCount, int[] Lifts, String above, String below, int user, int minIndex)
+        {
+            for (int i = 0; i < Floors; i++)
             {
                 for (int j = 0; j < LiftCount; j++)
                 {
                     if (Lifts[j] == i)
                     {
-                        Console.BackgroundColor = ConsoleColor.Green;
+                        if(j == minIndex){
+                            Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        }
+                        else{
+                            Console.BackgroundColor = ConsoleColor.Red;
+                        }
                         Console.Write(above);
                         Console.ResetColor();
                     }
@@ -46,10 +59,42 @@ namespace opdracht_3
                         Console.Write(above);
                     }
                 }
-                Console.WriteLine();
+                if (i == user)
+                {
+                    Console.WriteLine($"  {above}");
+                }
+                else
+                {
+                    Console.WriteLine("");
+                }
+
+                for (int j = 0; j < LiftCount; j++)
+                {
+                    if (Lifts[j] == i)
+                    {
+                        if(j == minIndex){
+                            Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        }
+                        else{
+                            Console.BackgroundColor = ConsoleColor.Red;
+                        }
+                        Console.Write(below);
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.Write(below);
+                    }
+                }
+                if (i == user)
+                {
+                    Console.WriteLine($"  {below}");
+                }
+                else
+                {
+                    Console.WriteLine("");
+                }
             }
-
-
         }
     }
 }
